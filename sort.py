e5798137b5fd9032538f02c295ea7b8432c190d1
Nikhil Menon <74824711+nike-7@users.noreply.github.com>
2021-01-13 09:25:08 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,1 +1,3 @@
-
+List = ['Design an Outfit for Ellie','!Light in your Kitchen','Use the Twilio API','Build a Data Visualization','Write Code to Sort a List','Submit a Challenge with Someone in your Guild']
+List.sort()
+print(List)
